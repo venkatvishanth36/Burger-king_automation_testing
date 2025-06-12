638f99cae291765276c0f50adfc5715083d4a81e
Venkatesh.B <115982698+venkatvishanth36@users.noreply.github.com>
2025-06-12 21:19:27 +0530

    code file
    code file

---
+++ homepage.java
@@ -0,0 +1,34 @@
+package burger_king;
+
+import org.openqa.selenium.manager.SeleniumManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class homepage {
+	public static void main(String[]args) {
+		
+		// Setup driver
+		WebDriver driver = new ChromeDriver();
+		
+		try {
+			driver.manage().window().maximize();
+			driver.get("https://www.bk.com/");
+
+			
+			// Wait for page to load, and print title
+			String Title = driver.getTitle();
+			System.out.println("Page Title: " + Title);
+			
+			if(Title.contains("Burger King")) {
+				System.out.println("✅ Homepage Loaded Successfully");
+			} else {
+				System.out.println("❌ Unexpected Page Title");
+			}
+		} catch (Exception e) {
+			System.out.println("❗ Error: " + e.getMessage());
+		}finally {
+			driver.quit();
+		}
+	}
+
+}
